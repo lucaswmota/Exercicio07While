f9c7621f809b81c3cb186df09e21fd3b4208b3d3
Lucas Mota <lucasmota7@gmail.com>
2021-11-29 20:20:15 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio07While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string resp = "S";
+            double valor, total = 0;
+
+            while (resp == "S")
+            {
+                Console.WriteLine("Informe o valor do produto");
+                valor = Double.Parse(Console.ReadLine());
+
+                total += valor;
+
+                Console.WriteLine("Deseja Inserir um novo produto?");
+                resp = Console.ReadLine().ToUpper();
+            }
+
+            Console.WriteLine("O valor final da venda é: " + total.ToString("C"));
+            Console.ReadKey();
+
+        }
+    }
+}
